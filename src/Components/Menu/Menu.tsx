c2e9e906e8fcb028194b73f1fe70f10e15f2994a
bachpro19 <bachxtnd2000@gmail.com>
2021-12-31 00:56:26 +0700

    fix menu and topbar

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import { Image } from '../Images/Image'
-import  "./menu.module.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import { Image } from "../Images/Image";
+import "./menu.module.css";
 import iconCommunity from "./img/icon-community.svg";
-import shoppingBasket from "./img/icon-shopping-basket.svg"
+import shoppingBasket from "./img/icon-shopping-basket.svg";
 import iconPool from "./img/icon-pool.svg";
 import iconFarms from "./img/icon-farms.svg";
 import iconMarket from "./img/icon-market.svg";
@@ -16,96 +16,155 @@ import iconHelp from "./img/icon-help.svg";
 import iconHouseHeart from "./img/icon-house-heart.svg";
 import iconAppDownload from "./img/icon-app-download.svg";
 import iAmBabu from "./img/i-am-babu.png";
-function Menu():JSX.Element  {
-    return (
-        <>
-            <div className="col-auto">
-            <aside>
-              <ul id="mainmenu" className="list-unstyled">
-                <li>
-                  <Link to="#" className="active">
-                    <img src={iconCommunity} className="icon" /><span className="text">Home</span>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="#">
-                    <img src={shoppingBasket} className="icon" /><span className="text">Buy $BABU</span>
-                  </Link>
-                </li>
-                <li className="parentMenu">
-                  <Link to="#nftSubmenu" data-bs-toggle="collapse">
-                    <Image src={iconPool} className="icon" /><span className="text">NFT Mining</span>
-                  </Link>
-                  <ul className="subMenu collapse list-unstyled" id="nftSubmenu">
-                    <li><Link to="#">Babbu Family NFT</Link></li>
-                    <li><Link to="#">Babbu Friends Card</Link></li>
-                    <li><Link to="#">Crystal Stone</Link></li>
-                    <li><Link to="#">Bounty</Link></li>
-                  </ul>
-                </li>
-                <li>
-                  <Link to="#">
-                    <Image src={iconFarms} className="icon" /><span className="text">Farms</span><span className="badge arrow bg-pink text-white">HOT</span>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="#">
-                    <Image src={iconMarket} className="icon" /><span className="text">NFT Market</span>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="#">
-                    <Image src={iconGamebase} className="icon" /><span className="text">Gamebase</span><span className="badge arrow bg-yellow">NEW</span>
-                  </Link>
-                </li>
-                <li className="parentMenu">
-                  <Link to="#babbuNftSubmenu" data-bs-toggle="collapse">
-                    <Image src={iconLogo} className="icon" /><span className="text">Babbu NFT</span>
-                  </Link>
-                  <ul className="subMenu collapse list-unstyled" id="babbuNftSubmenu">
-                    <li><Link to="#">Babbu NFT Box</Link></li>
-                    <li><Link to="#">NFT Mint</Link></li>
-                  </ul>
-                </li>
-                <li>
-                  <Link to="#">
-                    <Image src={iconBounty} className="icon" /><span className="text">Bounty</span>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="#">
-                    <Image src={iconKnight} className="icon" /><span className="text">Guide</span><span className="badge arrow bg-grey">COMING SOON</span>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="#">
-                    <Image src={iconThumbsUp} className="icon" /><span className="text">Community</span>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="#">
-                    <Image src={iconHelp} className="icon" /><span className="text">Help Center</span>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="#">
-                    <Image src={iconHouseHeart} className="icon" /><span className="text">About BABBU</span>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="#">
-                    <Image src={iconAppDownload} className="icon" /><span className="text">App Download</span>
-                  </Link>
-                </li>
-              </ul>
-              <div className="iAmBabu">
-                <Image src={iAmBabu} alt="" />
-              </div>
-            </aside>
-          </div>
-        </>
-    )
+
+const submenuData = [
+  {
+    title: "Home",
+    icon: iconCommunity,
+    path: "/",
+  },
+  {
+    title: "Buy $BABU",
+    icon: shoppingBasket,
+    path: "#",
+  },
+  {
+    title: "NFT Mining",
+    icon: iconPool,
+    path: "#",
+    subNav: [
+      {
+        title: "Babbu Family NFT",
+        path: "#",
+      },
+      {
+        title: "Babbu Friends Card",
+        path: "#",
+      },
+      {
+        title: "Crystal Stone",
+        path: "#",
+      },
+      {
+        title: "Bounty",
+        path: "#",
+      },
+    ],
+  },
+  {
+    title: "Farm",
+    icon: iconFarms,
+    path: "#",
+  },
+  {
+    title: "NFT Market",
+    icon: iconMarket,
+    path: "#",
+  },
+  {
+    title: "Gamebase",
+    icon: iconGamebase,
+    path: "#",
+
+    new: true,
+  },
+  {
+    title: "Babbu NFT",
+    icon: iconLogo,
+    path: "#",
+    subNav: [
+      {
+        title: "Babbu NFT Box",
+        path: "#",
+      },
+      {
+        title: "NFT Mint",
+        path: "#",
+      },
+    ],
+  },
+  {
+    title: "Bounty",
+    icon: iconBounty,
+    path: "#",
+  },
+  {
+    title: "Guide",
+    icon: iconKnight,
+    path: "#",
+
+    comingsoon: true,
+  },
+  {
+    title: "Community",
+    icon: iconThumbsUp,
+    path: "#",
+  },
+  {
+    title: "Help Center",
+    icon: iconHelp,
+    path: "#",
+  },
+  {
+    title: "About BABU",
+    icon: iconHouseHeart,
+    path: "#",
+  },
+  {
+    title: "App Dowload",
+    icon: iconAppDownload,
+    path: "#",
+  },
+];
+
+function SubMenu(data: any): JSX.Element {
+  const { item } = data;
+  const [subnav, setSubnav] = React.useState(false);
+
+  const showSubnav = () => setSubnav(!subnav);
+  return (
+    <li className={item.subNav ? "parentMenu" : ""}>
+      <Link to={item.path} onClick={item.subNav && showSubnav}>
+        <img src={item.icon} className="icon" />
+        <span className="text">{item.title}</span>
+      </Link>
+      {item.subNav ? (
+        <ul
+          className={
+            subnav
+              ? "subMenu collapse list-unstyled show"
+              : "subMenu collapse list-unstyled"
+          }
+          id="nftSubmenu"
+        >
+          {item.subNav.map((subItem: any, subIndex: any) => (
+            <li key={subIndex}>
+              <Link to={subItem.path}>{subItem.title}</Link>
+            </li>
+          ))}
+        </ul>
+      ) : null}
+    </li>
+  );
 }
 
-export default Menu
+function Menu(): JSX.Element {
+  return (
+    <>
+      <div className="col-auto">
+        <aside>
+          <ul id="mainmenu" className="list-unstyled">
+            {submenuData.map((item: any, index: any) => (
+              <SubMenu item={item} key={index} />
+            ))}
+          </ul>
+          <div className="iAmBabu">
+            <Image src={iAmBabu} alt="" />
+          </div>
+        </aside>
+      </div>
+    </>
+  );
+}
 
+export default Menu;
